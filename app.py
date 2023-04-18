b0b7b9fd308d1ca7d17ece7055bfa0a69b56e83b
Justin Pakzad <justinpak303@gmail.com>
2023-04-18 12:59:12 +0200

    new

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 import sys
 import os
-import time
-import sqlalchemy
-from sqlalchemy import text
-from google.oauth2 import service_account
-from google.cloud.sql.connector import Connector, IPTypes
-import pg8000
+from database import make_query
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask.logging import create_logger
 import logging
-import psycopg2  # Add this line
 from dotenv import load_dotenv  # Add this line
 import json
 # from database import conn_pool
@@ -35,88 +29,9 @@ def about():
     return render_template("about.html")
 
 
-credentials_json = os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"]
-credentials_dict = json.loads(credentials_json)
-credentials = service_account.Credentials.from_service_account_info(credentials_dict)
-
-def connect_with_connector() -> sqlalchemy.engine.base.Engine:
-    """
-    Initializes a connection pool for a Cloud SQL instance of Postgres.
-
-    Uses the Cloud SQL Python Connector package.
-    """
-    # Note: Saving credentials in environment variables is convenient, but not
-    # secure - consider a more secure solution such as
-    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-    # keep secrets safe.
-
-    instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]  # e.g. 'project:region:instance'
-    db_user = os.environ["USER_DB"]  # e.g. 'my-db-user'
-    db_pass = os.environ["PASSWORD"]  # e.g. 'my-db-password'
-    db_name = os.environ["DATABASE_NAME"]  # e.g. 'my-database'
-
-    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
-
-    # initialize Cloud SQL Python Connector object
-    connector = Connector(credentials=credentials)
-
-    def getconn() -> pg8000.dbapi.Connection:
-        LOG.debug("Starting connection attempt")
-        conn: pg8000.dbapi.Connection = connector.connect(
-            instance_connection_name,
-            "pg8000",
-            user=db_user,
-            password=db_pass,
-            db=db_name,
-            ip_type=ip_type,
-        )
-        return conn
-
-    # The Cloud SQL Python Connector can be used with SQLAlchemy
-    # using the 'creator' argument to 'create_engine'
-    pool = sqlalchemy.create_engine(
-        "postgresql+pg8000://",
-        creator=getconn,
-        # ...
-    )
-    return pool
-
-    # connection = None
-    # LOG.debug("Starting connection attempt")
-    # start_time = time.time()
-    # try:
-    #     LOG.debug(os.environ.get('HOST'))
-    #     LOG.debug(os.environ.get('DATABASE_NAME'))
-    #     LOG.debug(os.environ.get('USER_DB'))
-    #     LOG.debug(os.environ.get('PASSWORD'))
-    #     LOG.debug(os.environ.get('DB_PORT'))
-    #     connection = psycopg2.connect(
-    #     host=os.environ.get('HOST'),
-    #     dbname=os.environ.get('DATABASE_NAME'),
-    #     user=os.environ.get('USER_DB'),
-    #     password=os.environ.get('PASSWORD'),
-    #     port=os.environ.get('DB_PORT', '5432'))
-    #     if connection:
-    #         end_time = time.time()
-    #         LOG.debug(f"Connection successful, time elapsed: {end_time - start_time} seconds")
-    #         return "Connection to the database is successful!"
-    # except Exception as exce:
-    #     end_time = time.time()
-    #     LOG.error(f"An error occurred while connecting to the database: {exce}, time elapsed: {end_time - start_time} seconds")
-    #     return f"An error occurred while connecting to the database: {exce}", 500
-    # finally:
-    #     if connection:
-    #         connection.close()
-
 @app.route("/test_db")
 def test_connection():
-    engine = connect_with_connector()
-    query = text("SELECT * FROM release_artist_trimmed LIMIT 5")
-    with engine.connect() as connection:
-        result = connection.execute(query).fetchall()
-        result = [tuple(row) for row in result]
-        json_string = json.dumps(result)
-    return json_string
+    return make_query("SELECT * FROM release_artist_trimmed LIMIT 5")
 
 
 @app.route("/search", methods=["GET", "POST"])
--- database.py
+++ database.py
@@ -1,16 +1,67 @@
-# import psycopg2
-# from psycopg2.pool import ThreadedConnectionPool
+import sqlalchemy
+from sqlalchemy import text
+from google.oauth2 import service_account
+from google.cloud.sql.connector import Connector, IPTypes
+import pg8000
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# import os
-# from dotenv import load_dotenv
+credentials_json = os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"]
+credentials_dict = json.loads(credentials_json)
+credentials = service_account.Credentials.from_service_account_info(credentials_dict)
 
-# load_dotenv()
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of Postgres.
 
-# conn_pool = ThreadedConnectionPool(
-#     1, 20,
-#     os.environ.get('HOST'),
-#     os.environ.get('DATABASE_NAME'),
-#     os.environ.get('USER_DB'),
-#     os.environ.get('PASSWORD'),
-#     os.environ.get('PORT')
-# )
+    Uses the Cloud SQL Python Connector package.
+    """
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+
+    instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]  # e.g. 'project:region:instance'
+    db_user = os.environ["USER_DB"]  # e.g. 'my-db-user'
+    db_pass = os.environ["PASSWORD"]  # e.g. 'my-db-password'
+    db_name = os.environ["DATABASE_NAME"]  # e.g. 'my-database'
+
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    # initialize Cloud SQL Python Connector object
+    connector = Connector(credentials=credentials)
+
+    def getconn() -> pg8000.dbapi.Connection:
+        LOG.debug("Starting connection attempt")
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=ip_type,
+        )
+        return conn
+
+    # The Cloud SQL Python Connector can be used with SQLAlchemy
+    # using the 'creator' argument to 'create_engine'
+    pool = sqlalchemy.create_engine(
+        "postgresql+pg8000://",
+        creator=getconn,
+        # ...
+    )
+    return pool
+
+def make_query(query):
+    engine = connect_with_connector()
+    query = text(query)
+    with engine.connect() as connection:
+        result = connection.execute(query).fetchall()
+        # Get the column names from the result set
+        column_names = result[0].keys()
+        # Convert the rows to dictionaries
+        rows = [dict(zip(column_names, row)) for row in result]
+        json_string = json.dumps(rows)
+    return json_string
